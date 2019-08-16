236165cd24e6678778ae4967bb5876c7c0f636ad
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-08-16 21:00:53 +0200

    Add unit tests for SetNewNumberAvailable method in Movie class

---
+++ Video Rental Shop.UnitTests/MovieTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Video_Rental_Shop.Models;
+
+namespace Video_Rental_Shop.UnitTests
+{
+    [TestFixture]
+    public class MovieTests
+    {
+        private Movie movieInDb;
+        private Movie editedMovie;
+
+        [SetUp]
+        public void SetUp()
+        {
+            movieInDb = new Movie();
+            editedMovie = new Movie();
+        }
+
+        [Test]
+        [TestCase(1, 1, 1, 1)]
+        [TestCase(1, 1, 2, 2)]
+        [TestCase(1, 1, 0, 0)]
+        [TestCase(1, 2, 3, 2)]
+        [TestCase(1, 2, 1, 0)]
+        public void SetNewNumberAvailable_WhenCalled_AssignCalculatedNewValueOfNumberAvailable(int movieInDbNumberAvailable, int movieInDbNumberInStock, int editedMovieNumberInStock, int expectedResult)
+        {
+            movieInDb.NumberAvailable = editedMovie.NumberAvailable = movieInDbNumberAvailable;
+            movieInDb.NumberInStock = movieInDbNumberInStock;
+            editedMovie.NumberInStock = editedMovieNumberInStock;
+
+            movieInDb.SetNewNumberAvailable(movieInDb, editedMovie);
+
+            Assert.That(movieInDb.NumberAvailable, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ Video Rental Shop.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Video Rental Shop.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Video Rental Shop.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("555e4669-4085-432c-9f52-0b2fb629e60a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
