502f7970d6f611f9c1d07afb9b707ef71f44d20d
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-08-25 19:25:50 +0200

    Add Roles enumerator

---
+++ Video Rental Shop/Enumerators/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Video_Rental_Shop.Enumerators
+{
+    public enum Roles
+    {
+        Manager,
+        Employee,
+        Guest
+    }
+}
